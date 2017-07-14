c0c4a2204b858123586e13f2d3f8730f5d9e2c20
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-14 11:47:04 +0100

    mailchimp consistency

--- includes/header.php
+++ includes/header.php
@@ -9,8 +9,8 @@
     <meta name=“keywords” content="south, west, tech, industry, job, jobs, positions, open, work, Gloucestershire, Bristol, Wiltshire, Somerset, Dorset, Devon, Cornwall, developer, designer, marketing, manager, engineer, frontend, backend, ui, ux, product, devops">
     <title>South West Tech Jobs</title>
     <link rel="icon" type="image/png" href="http://swtechjobs/img/favicon.png" />
-    <link rel="stylesheet" href="css/style.css">
-    <link href="css/mailchimp.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
 
     <meta property="og:site_name" content="South West Tech Jobs">
     <meta property="og:title" content="South West Tech Jobs">
--- index.php
+++ index.php
@@ -12,23 +12,18 @@
       </div>
       <p>We are building a dedicated job board for the rapidly growing Tech Industry in the South West. Traditional job sites are not tailored for us, therefore this board will be focusing on Engineering, Design, Product, DevOps, Marketing and more.</p>
 
-    </div>
-
-    <!-- Begin MailChimp Signup Form -->
-
-    <div id="mc_embed_signup">
-
-      <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup">
+        <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
           <div id="mc_embed_signup_scroll">
-
-      	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
-          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
-          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
           </div>
-      </form>
+        </form>
+      </div>
+
     </div>
 
-    <!--End mc_embed_signup-->
 
     <script src="js/jquery.js"></script>
     <script src="js/typed.js"></script>
