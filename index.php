d5c6c20d03956326676d4d549017d9578d951d24
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-15 09:28:51 +0100

    container changes

--- index.php
+++ index.php
@@ -2,31 +2,31 @@
 
     <!-- <a href="https://twitter.com/swtechjobs" class="twitter-follow-button" data-show-count="false">Follow @swtechjobs</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script> -->
     <div class="header-container">
-    <div class="header">
+      <div class="header">
 
-      <div class="header__navigation">
-        <img src="img/swtechjobs.svg" alt="South West Tech Jobs">
-        <h1>South West Tech Jobs</h1>
-        <a href="#" class="button">Post a job for FREE<a/>
+        <div class="header__navigation">
+          <img src="img/swtechjobs.svg" alt="South West Tech Jobs">
+          <h1>South West Tech Jobs</h1>
+          <a href="#" class="button">Post a job for FREE</a>
+        </div>
+
+        <div class="header__headline">
+          <h2>Looking for a position as </br><span class="element"></span></h2>
+        </div>
+        <p>Get a weekly email with the latest tech jobs</p>
+
+        <div id="mc_embed_signup">
+          <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
+          <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+          </form>
+        </div>
+
       </div>
-
-      <div class="header__headline">
-        <h2>Looking for a position as </br><span class="element"></span></h2>
-      </div>
-      <p>Get a weekly email with the latest tech jobs</p>
-
-      <div id="mc_embed_signup">
-        <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
-        <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-          <div id="mc_embed_signup_scroll">
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
-            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-          </div>
-        </form>
-      </div>
-
-    </div>
     </div>
 
     <div class="job-container">
