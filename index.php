429aed018badc376f8283d386fa438a719e9c19e
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-17 14:18:06 +0100

    Added google form

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <h1 class="hide-for-small">South West Tech Jobs</h1>
             <h1 class="hide-for-large">SW Tech Jobs</h1>
           </a>
-          <a href="#" class="button">Post a job for FREE</a>
+          <a href="https://goo.gl/forms/NAJaCim87WGa2dJT2" target="_blank" class="button">Post a job for FREE</a>
         </div>
 
         <div class="header__headline">
@@ -36,8 +36,8 @@
 
         <a href="#" class="job">
           <div class="job__title">
-            <h3>Digital Designer</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
+            <h3>Front End Engineer</h3>
+            <p>VentureHarbour - Remote</p>
           </div>
 
           <div class="job__timestamp">
@@ -58,7 +58,7 @@
 
         <a href="#" class="job">
           <div class="job__title">
-            <h3>Digital Designer</h3>
+            <h3>Full Stack Developer</h3>
             <p>Pro:Direct Sport - Newton Abbot</p>
           </div>
 
@@ -69,7 +69,7 @@
 
         <a href="#" class="job">
           <div class="job__title">
-            <h3>Digital Designer</h3>
+            <h3>Videographer</h3>
             <p>Pro:Direct Sport - Newton Abbot</p>
           </div>
 
@@ -80,18 +80,7 @@
 
         <a href="#" class="job">
           <div class="job__title">
-            <h3>Digital Designer</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
-
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Digital Designer</h3>
+            <h3>Content Editor</h3>
             <p>Pro:Direct Sport - Newton Abbot</p>
           </div>
 
