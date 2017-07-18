b5b9bb88a88e6f0bec76a77d156df2bfc10fbac3
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-18 08:28:15 +0100

    moved jobs list into include

---
+++ includes/jobs.php
@@ -0,0 +1,54 @@
+<a href="#" class="job">
+  <div class="job__title">
+    <h3>Front End Engineer</h3>
+    <p>VentureHarbour - Exeter/Remote</p>
+  </div>
+
+  <div class="job__timestamp">
+    <p>July 14</p>
+  </div>
+</a>
+
+<a href="#" class="job">
+  <div class="job__title">
+    <h3>Digital Designer</h3>
+    <p>Pro:Direct Sport - Newton Abbot</p>
+  </div>
+
+  <div class="job__timestamp">
+    <p>July 14</p>
+  </div>
+</a>
+
+<a href="#" class="job">
+  <div class="job__title">
+    <h3>Full Stack Developer</h3>
+    <p>Pro:Direct Sport - Newton Abbot</p>
+  </div>
+
+  <div class="job__timestamp">
+    <p>July 14</p>
+  </div>
+</a>
+
+<a href="#" class="job">
+  <div class="job__title">
+    <h3>Videographer</h3>
+    <p>Pro:Direct Sport - Newton Abbot</p>
+  </div>
+
+  <div class="job__timestamp">
+    <p>July 14</p>
+  </div>
+</a>
+
+<a href="#" class="job">
+  <div class="job__title">
+    <h3>Content Editor</h3>
+    <p>Pro:Direct Sport - Newton Abbot</p>
+  </div>
+
+  <div class="job__timestamp">
+    <p>July 14</p>
+  </div>
+</a>
--- index.php
+++ index.php
@@ -34,60 +34,7 @@
     <div class="bg-color">
       <div class="job-container">
 
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Front End Engineer</h3>
-            <p>VentureHarbour - Exeter/Remote</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
-
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Digital Designer</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
-
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Full Stack Developer</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
-
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Videographer</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
-
-        <a href="#" class="job">
-          <div class="job__title">
-            <h3>Content Editor</h3>
-            <p>Pro:Direct Sport - Newton Abbot</p>
-          </div>
-
-          <div class="job__timestamp">
-            <p>July 14</p>
-          </div>
-        </a>
+        <?php include "includes/jobs.php"; ?>
 
         <a href="https://twitter.com/swtechjobs" class="twitter-follow-button" data-show-count="false">Follow @swtechjobs</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 
