6828118849d631c1651ae9d8cf9f43704e0b1711
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-19 19:44:36 +0100

    implemented job page design

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,16 @@
 <div class="footer">
 
+  <div id="mc_embed_signup">
+    <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
+    <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Subscribe for job updates" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      </div>
+    </form>
+  </div>
+
   <div class="footer__signoff">
     Contact us <a href="mailto:info@swtechjobs.com">here</a> &#128075; . Created in Torbay by <a href="http://andrewgormley.co.uk/" target="_blank">Andrew Gormley</a>. Copyright © 2017.
   </div>
--- venture-harbour-front-end-developer.php
+++ venture-harbour-front-end-developer.php
@@ -8,18 +8,8 @@
             <img src="img/swtechjobs.svg" alt="South West Tech Jobs">
             <h1 class="hide-for-small">South West Tech Jobs</h1>
             <h1 class="hide-for-large">SW Tech Jobs</h1>
+            <a href="https://goo.gl/forms/NAJaCim87WGa2dJT2" target="_blank" class="button">Post a job for FREE</a>
           </a>
-        </div>
-
-        <div id="mc_embed_signup">
-          <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
-          <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
-              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
-              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
-          </form>
         </div>
 
       </div>
@@ -29,83 +19,66 @@
       <div class="jobAdvert">
         <div class="jobAdvert__heading">
           <h2>Front End Web Engineer</h2>
-          <p>Venture Harbour Exeter/Remote</p>
+          <p><a href="">Venture Harbour</a> - Exeter/Remote</p>
         </div>
         <div class="jobAdvert__body">
-          <p>
-            se your extensive knowledge of JavaScript, HTML, and CSS to improve our continually growing portfolio of ventures. The ventures are varied and built using all kinds of platforms, and this will require you to be flexible with your skillset and learning, but also adaptable to a quick changing environment. You will collaborate closely with everyone in the business, from design and devops to marketing and customer support. This role will report to the Head of Product.
 
-Day-to-day
-Pick up a task, and deliver quality code
+          <p>Use your extensive knowledge of JavaScript, HTML, and CSS to improve our continually growing portfolio of ventures. The ventures are varied and built using all kinds of platforms, and this will require you to be flexible with your skillset and learning, but also adaptable to a quick changing environment. You will collaborate closely with everyone in the business, from design and devops to marketing and customer support. This role will report to the Head of Product.</p>
 
-Lead us in best practices and processes
+          <h3>Day-to-day</h3>
 
-Create detailed requirements where they don’t already exist, with the help of the team
+          <ul>
+            <li>Pick up a task, and deliver quality code</li>
+            <li>Lead us in best practices and processes</li>
+            <li>Create detailed requirements where they don’t already exist, with the help of the team</li>
+            <li>Work with external dev team to ensure your code fits in with the overall design and architecture of the stack</li>
+            <li>Contribute ideas and innovations, wherever you see opportunity for improvement</li>
+            <li>Work collaboratively with our multi-disciplinary team</li>
+            <li>Continually innovate and evolve the frontend of all our ventures</li>
+            <li>Think through customer requirements to come up with high-impact ways of quickly solving their problems</li>
+            <li>Empathize with the many different types of users</li>
+            <li>Work smart</li>
+          </ul>
 
-Work with external dev team to ensure your code fits in with the overall design and architecture of the stack
+          <h3>You may be a fit for this role if you:</h3>
 
-Contribute ideas and innovations, wherever you see opportunity for improvement
+          <ul>
+            <li>Love problem solving</li>
+            <li>Enjoy build complex responsive websites in HTML and CSS</li>
+            <li>Have a high bar for quality of code and quality of user experience</li>
+            <li>Like the challenges of debugging and fix all kinds of issues</li>
+            <li>Are keen to use SASS or LESS because of what it offers</li>
+            <li>Have tasted and found that pattern libraries are good</li>
+            <li>Enjoy using frameworks such as Angular, React, Vue, Ember or Meteor</li>
+            <li>Strive to improve JavaScript performance</li>
+            <li>Understand the importance of building and writing unit, acceptance, visual regression and performance tests</li>
+            <li>Are keen to work with modern/feature browser technologies</li>
+            <li>Have design sensibilities, and a desire to sweat the small stuff, but also understands the need for progress over perfection</li>
+            <li>Have strong communication skills, a positive attitude, and empathy</li>
+            <li>Are self-­awareness and have a desire to continually improve</li>
+            <li>Are humble and a team player</li>
+            <li>Can have a good laugh and be yourself :)</li>
+          </ul>
 
-Work collaboratively with our multi-disciplinary team
+          <p>And also can get alongside and get excited about our values.</p>
 
-Continually innovate and evolve the frontend of all our ventures
+          <h3>At Venture Harbour</h3>
 
-Think through customer requirements to come up with high-impact ways of quickly solving their problems
+          <h4>We believe</h4>
+          <p>In raising the bar beyond exceptional. We’re a small, focused team passionate about doing work that we’re proud to put our name to. The pace is fast, the work is stimulating, and innovation is expected. We hire the best and brightest people, and offer one of Oxfordshire and Berkshire’s top workplaces.</p>
 
-Empathize with the many different types of users
+          <h4>Work with the best people</h4>
+          <p>If you join Venture Harbour, you’ll work alongside ex-Google engineers who’ll teach you the ins and outs of coding, jiu-jitsu world championship fighters that’ll show you how to strategise a product launch, and more. You’ll be surrounded by incredible people that’ll teach you things you could never dream of.</p>
 
-Work smart
+          <h4>Turn ideas into reality</h4>
+          <p>Our team specialises in creating and growing online businesses. We have a portfolio of eight ventures in industries ranging from luxury travel to the music industry. Our team is constantly brainstorming ideas for new venture ideas to bring to life. In addition to bringing our own ideas to life, we also collaborate with a small number of exciting businesses to bring their ideas to life too. We’ve worked with the likes of Universal Records, McLaren, and Johnnie Walker to help them achieve exceptional results through digital marketing.</p>
 
-You may be a fit for this role if you:
-
-Love problem solving
-
-Enjoy build complex responsive websites in HTML and CSS
-
-Have a high bar for quality of code and quality of user experience
-
-Like the challenges of debugging and fix all kinds of issues
-
-Are keen to use SASS or LESS because of what it offers
-
-Have tasted and found that pattern libraries are good
-
-Enjoy using frameworks such as Angular, React, Vue, Ember or Meteor
-
-Strive to improve JavaScript performance
-
-Understand the importance of building and writing unit, acceptance, visual regression and performance tests
-
-Are keen to work with modern/feature browser technologies
-
-Have design sensibilities, and a desire to sweat the small stuff, but also understands the need for progress over perfection
-
-Have strong communication skills, a positive attitude, and empathy
-
-Are self-­awareness and have a desire to continually improve
-
-Are humble and a team player
-
-Can have a good laugh and be yourself :)
-And also can get alongside and get excited about our values.
-
-At Venture Harbour
-
-We believe
-
-In raising the bar beyond exceptional. We’re a small, focused team passionate about doing work that we’re proud to put our name to. The pace is fast, the work is stimulating, and innovation is expected. We hire the best and brightest people, and offer one of Oxfordshire and Berkshire’s top workplaces.
+          <div class="apply">
+            <div class="apply__title">How to apply for this role</div>
+            <p>Submit your details, resume and cover letter via this <a href="https://venture-harbour.breezy.hr/p/b99c979c796501-front-end-engineer/apply">application form</a>.</p>
+          </div>
 
 
-Work with the best people
-
-If you join Venture Harbour, you’ll work alongside ex-Google engineers who’ll teach you the ins and outs of coding, jiu-jitsu world championship fighters that’ll show you how to strategise a product launch, and more. You’ll be surrounded by incredible people that’ll teach you things you could never dream of.
-
-Turn ideas into reality
-
-Our team specialises in creating and growing online businesses. We have a portfolio of eight ventures in industries ranging from luxury travel to the music industry. Our team is constantly brainstorming ideas for new venture ideas to bring to life.
-
-In addition to bringing our own ideas to life, we also collaborate with a small number of exciting businesses to bring their ideas to life too. We’ve worked with the likes of Universal Records, McLaren, and Johnnie Walker to help them achieve exceptional results through digital marketing.
-          </p>
         </div>
       </div>
     </div>
