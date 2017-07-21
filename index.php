6584520594b5d1845addc90f58259dcbb465a63f
andrewgormley <andrew.gormley@crowdcube.com>
2017-07-21 11:21:38 +0100

    removed duplicate mail chimp forms

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
         </div>
         <p>Subscribe for a weekly email with the latest jobs.</p>
 
-        <div class="mc_embed_signup">
+        <div id="mc_embed_signup">
           <link rel="stylesheet" href="css/mailchimp.css" type="text/css">
-          <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;class=46045080ec" method="post" class="mc-embedded-subscribe-form validate" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-            <div class="mc_embed_signup_scroll">
-              <input type="email" value="" name="EMAIL" class="email mce-EMAIL" placeholder="Email address" required>
+          <form action="//swtechjobs.us16.list-manage.com/subscribe/post?u=3ea77013099b912717ea670d7&amp;id=46045080ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3ea77013099b912717ea670d7_46045080ec" tabindex="-1" value=""></div>
-              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" class="mc-embedded-subscribe button"></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
             </div>
           </form>
         </div>
@@ -29,6 +29,15 @@
 
         <?php include "includes/jobs.php"; ?>
 
+      </div>
+    </div>
+
+    <div class="footer">
+
+      <a href="https://twitter.com/swtechjobs" class="twitter-follow-button" data-show-count="false">Follow @swtechjobs</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+      <div class="footer__signoff">
+        Contact us <a href="mailto:info@swtechjobs.com">here</a>. Created in Torbay by <a href="http://andrewgormley.co.uk/" target="_blank">Andrew Gormley</a>. Copyright © 2017.
       </div>
     </div>
 
@@ -46,4 +55,5 @@
       });
     </script>
 
-<?php include "includes/footer.php"; ?>
+</body>
+</html>
